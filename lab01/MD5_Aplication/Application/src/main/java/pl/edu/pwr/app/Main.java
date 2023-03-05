0780f3ef98d619478a067fa374d08ee2f3ed1de4
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-03-05 12:41:14 +0100

    add new version of fronted

--- lab01/MD5_Aplication/Application/src/main/java/pl/edu/pwr/app/Main.java
+++ lab01/MD5_Aplication/Application/src/main/java/pl/edu/pwr/app/Main.java
@@ -5,8 +5,6 @@ import pl.edu.pwr.lib.Printer;
 import pl.edu.pwr.lib.MainWindow;
 public class Main {
     public static void main(String[] args) {
-        Printer printer = new Printer();
-        printer.print();
 
         MainWindow window = new MainWindow();
 
--- lab01/MD5_Aplication/Library/src/main/java/pl/edu/pwr/lib/MainWindow.java
+++ lab01/MD5_Aplication/Library/src/main/java/pl/edu/pwr/lib/MainWindow.java
@@ -1,35 +1,81 @@
 package pl.edu.pwr.lib;
+
+// Java program to show Example of CardLayout.
+// in java. Importing different Package.
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.event.*;
-import java.io.PrintStream;
-import java.io.FileOutputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
-public class MainWindow extends JFrame{
+// class extends JFrame
+public class MainWindow extends JFrame {
 
+
+    // Declaration of objects
+    // of CardLayout class.
+    private CardLayout cl;
     FileHandler fHander = new FileHandler();
-   // static JList fileList;
-    public MainWindow() {
-        // The data
+    public MainWindow()
+    {
         fHander.fillFilesList();
-        //JList fileList = new JList<String>();
-        JFrame frame = new JFrame();
-        frame.setSize(400, 200);
 
-        frame.add( new JScrollPane( new JList(fHander.getFiles().toArray())));
+        setTitle("Aplikacja MD5");
+        setSize(320, 200);
+        JPanel cardPanel = new JPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
 
-        frame.add( new JButton("Usuń pamięć programu"){{
-            addActionListener( new ActionListener() {
+        cl = new CardLayout();
+        cardPanel.setLayout(cl);
+        JPanel jp = new JPanel();
+        cardPanel.add(new JScrollPane( new JList(fHander.getFiles().toArray())));
 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("Usunieto");
-                }
-            });
-        }}, BorderLayout.SOUTH);
-        //frame.pack();
-        frame.setVisible( true );
+        JPanel buttonPanel = new JPanel();
+        JButton firstBtn = new JButton("First");
+        JButton nextBtn = new JButton("Next");
+        JButton previousBtn = new JButton("Previous");
+        JButton lastBtn = new JButton("Last");
+
+        buttonPanel.add(firstBtn);
+        buttonPanel.add(nextBtn);
+        buttonPanel.add(previousBtn);
+        buttonPanel.add(lastBtn);
+
+        // add ActionListeners
+        firstBtn.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent arg0)
+            {
+                System.out.println("first");
+            }
+        });
+
+        lastBtn.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent arg0)
+            {
+                System.out.println("last");
+            }
+        });
+
+        nextBtn.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent arg0)
+            {
+                System.out.println("next");
+            }
+        });
+
+        previousBtn.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent arg0)
+            {
+                System.out.println("prev");
+
+            }
+        });
+
+        getContentPane().add(cardPanel, BorderLayout.NORTH);
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
     }
 }
