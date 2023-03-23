2bec17a4dce3e1f1c9bc4a3844233711e456318c
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-03-23 12:48:06 +0100

    create class loader project

---
+++ lab04/ClassLoaderApp/src/main/java/Main.java
@@ -0,0 +1,11 @@
+package pl.pwr.edu.window;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
+
+    }
+}
---
+++ lab04/ClassLoaderApp/src/main/java/MainWindow.java
@@ -0,0 +1,13 @@
+package pl.pwr.edu.window;
+
+import pl.pwr.edu.music.ArtistLoader;
+
+
+public class MainWindow extends JFrame {
+
+
+    public MainWindow() {
+
+    }
+
+}
