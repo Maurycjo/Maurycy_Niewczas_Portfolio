adbb142d990cdfaee8ff2072316bbd839174c2e6
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-05-05 13:33:51 +0200

    enity, repository and service

---
+++ lab06/InternetApp/src/main/java/pl/edu/pwr/internetapp/service/iChargeService.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.internetapp.service;
+
+import pl.edu.pwr.internetapp.entity.Charge;
+import pl.edu.pwr.internetapp.entity.Installation;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface iChargeService {
+    List<Charge> getInstallationCharges(Long installationId);
+    Charge addCharge(LocalDate paymentDeadline, Float amountToPay, Boolean isPaid, Installation installationId);
+    void updateChargePaid(Long id, LocalDate paymentDeadline, Float amountToPay, Boolean isPaid, Installation installationId);
+}
