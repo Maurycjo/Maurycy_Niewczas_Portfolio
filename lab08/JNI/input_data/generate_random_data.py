10e5f4576efbdf67b37b19449a36874f46563b1b
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-05-22 18:34:13 +0200

    final version

--- lab08/JNI/input_data/generate_random_data.py
+++ lab08/JNI/input_data/generate_random_data.py
@@ -1,18 +1,17 @@
 import random
 
 
-def generate_random_data(filename="random_data.txt", min_size = 500, max_size = 500):
-    size = random.randint(min_size, max_size)
-
+def generate_random_data(filename="random_data.txt", size = 500):
+    
     with open(filename, 'w') as writer:
         writer.write(str(size))
         writer.write("\n")
 
         for j in range(0, size):
             for i in range(0, size-1):
-                writer.write(str(random.uniform(0, 50)))
+                writer.write(str(random.uniform(0, 1000)))
                 writer.write(",")
-            writer.write(str(random.uniform(0, 50)))
+            writer.write(str(random.uniform(0, 1000)))
             writer.write("\n")
     return size
 
@@ -21,5 +20,5 @@ if __name__ =="__main__":
 
     for j in range(100, 1001, 100):
         for i in range(0, 5):
-            generate_random_data(filename=str(j)+"_"+str(i) + ".txt")
+            generate_random_data(filename=str(j)+"_"+str(i) + ".txt", size=j)
             
--- lab08/JNI/library/java_class_JniAlgorithmLoader.cpp
+++ lab08/JNI/library/java_class_JniAlgorithmLoader.cpp
@@ -1,11 +1,11 @@
 #include <jni.h>
 #include "java_class_JniAlgorithmLoader.h"
 
-JNIEXPORT jobjectArray JNICALL Java_java_1class_JniAlgorithmLoader_loadAlgorithm(JNIEnv* env, jobject obj, jobjectArray array1, jobjectArray array2)
+JNIEXPORT jobjectArray JNICALL Java_java_1class_JniAlgorithmLoader_loadAlgorithm(JNIEnv* env, jobject obj, jobjectArray data, jobjectArray kernel)
 {
     // Get size of arrays
-    jsize size1 = env->GetArrayLength(array1);
-    jsize size2 = env->GetArrayLength(array2);
+    int size1 = env->GetArrayLength(data);
+    int size2 = env->GetArrayLength(kernel);
 
     // Check if arrays have the same size
     if (size1 != size2) {
@@ -13,50 +13,51 @@ JNIEXPORT jobjectArray JNICALL Java_java_1class_JniAlgorithmLoader_loadAlgorithm
     }
 
     // Get row size
-    jsize rowSize = 0;
-    if (size1 > 0) {
-        jobject firstRow = env->GetObjectArrayElement(array1, 0);
-        jdoubleArray doubleArray = static_cast<jdoubleArray>(firstRow);
-        rowSize = env->GetArrayLength(doubleArray);
-    }
+    int rowSize = 0;
+    
+    jdoubleArray firstRow = (jdoubleArray)env->GetObjectArrayElement(data, 0);
+    rowSize = env->GetArrayLength(firstRow);
+    env->DeleteLocalRef(firstRow);
+    
 
     // Create the result array
     jclass doubleArrayClass = env->FindClass("[D");
     jobjectArray resultArray = env->NewObjectArray(size1, doubleArrayClass, nullptr);
 
     // Perform convolution for each row
-    for (jsize i = 0; i < size1; i++) {
+    for (int i = 0; i < size1; i++) {
         // Get row from the first array
-        jobject inputRow = env->GetObjectArrayElement(array1, i);
-        jdoubleArray inputArray = static_cast<jdoubleArray>(inputRow);
-        jdouble* inputData = env->GetDoubleArrayElements(inputArray, nullptr);
+        jdoubleArray inputRow = (jdoubleArray)env->GetObjectArrayElement(data, i);
+        jdouble* inputData = env->GetDoubleArrayElements(inputRow, nullptr);
 
         // Get row from the second array
-        jobject kernelRow = env->GetObjectArrayElement(array2, i);
-        jdoubleArray kernelArray = static_cast<jdoubleArray>(kernelRow);
-        jdouble* kernelData = env->GetDoubleArrayElements(kernelArray, nullptr);
+        jdoubleArray kernelRow = (jdoubleArray)env->GetObjectArrayElement(kernel, i);
+        jdouble* kernelData = env->GetDoubleArrayElements(kernelRow, nullptr);
 
         // Create result row
         jdoubleArray resultRow = env->NewDoubleArray(rowSize);
-        jdouble* resultData = env->GetDoubleArrayElements(resultRow, nullptr);
+        jdouble* resultData = new jdouble[rowSize];
 
         // Calculate convolution for row
-        for (jsize j = 0; j < rowSize; j++) {
+        for (int j = 0; j < rowSize; j++) {
             resultData[j] = 0.0;
 
-            for (jsize k = 0; k < rowSize; k++) {
+            for (int k = 0; k < rowSize; k++) {
                 // Calculate convolution for element (i, j)
                 resultData[j] += inputData[k] * kernelData[rowSize - k - 1];
             }
         }
 
         // Set row to the result array
+        env->SetDoubleArrayRegion(resultRow, 0, rowSize, resultData);
         env->SetObjectArrayElement(resultArray, i, resultRow);
 
         // Release memory
-        env->ReleaseDoubleArrayElements(inputArray, inputData, 0);
-        env->ReleaseDoubleArrayElements(kernelArray, kernelData, 0);
-        env->ReleaseDoubleArrayElements(resultRow, resultData, 0);
+        env->ReleaseDoubleArrayElements(inputRow, inputData, JNI_ABORT);
+        env->ReleaseDoubleArrayElements(kernelRow, kernelData, JNI_ABORT);
+        env->DeleteLocalRef(inputRow);
+        env->DeleteLocalRef(kernelRow);
+        delete[] resultData;
     }
 
     return resultArray;
