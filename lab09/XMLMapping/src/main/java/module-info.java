57d184e9c176136b571f52d309741a2c5e60e76e
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-05-28 16:03:08 +0200

    simple gui with fileChooser in java fx

---
+++ lab09/XMLMapping/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module XMLMapping {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    exports pwr.edu.pl.window to javafx.graphics, javafx.fxml;
+    opens pwr.edu.pl.window to javafx.fxml;
+
+}
