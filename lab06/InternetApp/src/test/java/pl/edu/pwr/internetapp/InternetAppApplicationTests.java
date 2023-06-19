b70110d283c0fcb62c310c139547a78c1ff7be3f
Maurycy Niewczas <maurycy.niewczas@gmail.com>
2023-06-19 11:33:50 +0200

    code refactor in lab06

---
+++ lab06/InternetApp/src/test/java/pl/edu/pwr/internetapp/InternetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.internetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
